50b264fb2f6055ee70786ac4d0bb267eaa184f31
Cooldude1208 <44828391+Cooldude1208@users.noreply.github.com>
2024-03-14 20:38:02 +0530

    Commiting First application

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_from_directory
+import os
+
+
+
+app = Flask(__name__,static_folder='frontend/build/static')
+
+@app.route('/')
+def index():
+    return send_from_directory(os.path.join(os.getcwd(), 'frontend', 'build'), 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
